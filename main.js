96902c68fa0e90339f8ebfda1c29a802d4b6a697
destroyerman <destroyerman@tutanota.com>
2023-10-04 16:30:22 +0200

    actualize with bindings

--- main.js
+++ main.js
@@ -6,117 +6,108 @@ import GUI from "lil-gui";
 import Stats from "stats.js";
 
 class Experience {
-    constructor(canvas) {
-        window.experience = this;
+  constructor(canvas) {
+    window.experience = this;
 
-        this.canvas = canvas;
+    this.canvas = canvas;
 
-        this.scene = new THREE.Scene();
+    this.scene = new THREE.Scene();
 
-        this.setMesh();
-        this.setCamera();
-        this.setDebug();
-        this.events();
-        this.setRenderer();
-        this.update();
-    }
+    this.setMesh();
+    this.setCamera();
+    this.setDebug();
+    this.events();
+    this.setRenderer();
+    this.update();
+  }
 
-    setMesh() {
-        const mesh = new THREE.Mesh(
-            new THREE.PlaneGeometry(1, 1, 32, 32),
-            new THREE.ShaderMaterial({
-                vertexShader: vertex,
-                fragmentShader: fragment,
-                uniforms: {
-                    uTime: { value: 0.0 },
-                    // uResolution: { value: new THREE.Vector4() },
-                },
-                side: THREE.DoubleSide,
-            })
-        );
+  setMesh() {
+    this.mesh = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.ShaderMaterial({
+        vertexShader: vertex,
+        fragmentShader: fragment,
+        uniforms: {
+          uTime: { value: 0.0 },
+          // uResolution: { value: new THREE.Vector4() },
+        },
+        side: THREE.DoubleSide,
+      })
+    );
 
-        this.scene.add(mesh);
-    }
+    this.scene.add(this.mesh);
+  }
 
-    setCamera() {
-        this.camera = new THREE.PerspectiveCamera(
-            75,
-            window.innerWidth / window.innerHeight,
-            0.1,
-            100
-        );
-        this.camera.position.set(0, 0, 1);
+  setCamera() {
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      100
+    );
+    this.camera.position.set(0, 1, 2);
 
-        this.controls = new OrbitControls(this.camera, this.canvas);
-    }
+    this.controls = new OrbitControls(this.camera, this.canvas);
+  }
 
-    events() {
-        window.addEventListener("resize", () => {
-            this.resize();
-        });
+  events() {
+    this.resize = this.resize.bind(this);
+    window.addEventListener("resize", this.resize);
+  }
 
-        document.addEventListener("visibilitychange", () => {
-            if (document.visibilityState === "hidden") {
-                this.clock.stop();
-            } else {
-                this.clock.start();
-            }
-        });
-    }
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
 
-    resize() {
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  }
 
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    }
+  setDebug() {
+    this.gui = new GUI();
+  }
 
-    setDebug() {
-        this.gui = new GUI();
-    }
+  setRenderer() {
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true,
+    });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    this.renderer.setClearColor("black");
+  }
 
-    setRenderer() {
-        this.renderer = new THREE.WebGLRenderer({
-            canvas: this.canvas,
-            antialias: true,
-        });
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-        this.renderer.setClearColor("black");
-    }
+  update() {
+    /**
+     * Stats
+     */
+    this.stats = new Stats();
+    this.stats.showPanel(0);
+    document.body.appendChild(this.stats.dom);
 
-    update() {
-        /**
-         * Stats
-         */
-        this.stats = new Stats();
-        this.stats.showPanel(0);
-        document.body.appendChild(this.stats.dom);
+    /**
+     * RAF
+     */
+    this.previousTime = 0;
 
-        /**
-         * RAF
-         */
-        this.clock = new THREE.Clock();
-        this.previousTime = 0;
+    this.tick = this.tick.bind(this);
+    requestAnimationFrame(this.tick);
+  }
 
-        window.requestAnimationFrame(() => {
-            this.tick();
-        });
-    }
+  tick(t) {
+    this.stats.begin();
 
-    tick() {
-        this.stats.begin();
+    this.elapsedTime = t / 1000;
+    this.deltaTime = this.elapsedTime - this.previousTime;
+    this.previousTime = this.elapsedTime;
 
-        this.elapsedTime = this.clock.getElapsedTime();
-        this.deltaTime = this.elapsedTime - this.previousTime;
-        this.previousTime = this.elapsedTime;
+    this.renderer.render(this.scene, this.camera);
 
-        this.renderer.render(this.scene, this.camera);
+    this.mesh.rotation.y += this.deltaTime;
 
-        this.stats.end();
-        window.requestAnimationFrame(() => this.tick());
-    }
+    this.stats.end();
+    requestAnimationFrame(this.tick);
+  }
 }
 
 const experience = new Experience(document.querySelector("canvas"));
